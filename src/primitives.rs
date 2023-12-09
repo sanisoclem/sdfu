5c01a8dea1ae78d5563a2b71f425900771b0ae1e
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2023-12-10 02:51:46 +1100

    add triangle primitive

--- src/primitives.rs
+++ src/primitives.rs
@@ -228,3 +228,53 @@ where
         (pa - (ba * h)).magnitude() - self.thickness
     }
 }
+
+#[derive(Clone, Copy, Debug)]
+pub struct Triangle<V, D> {
+    pub points: [V; 3],
+    _pd: PhantomData<D>,
+}
+
+impl<V, D> Triangle<V, D> {
+    pub fn new(points: [V; 3]) -> Self {
+        Triangle {
+            points,
+            _pd: PhantomData,
+        }
+    }
+}
+
+impl<V> SDF<f32, V> for Triangle<V, Dim2D>
+where
+    V: Vec2<f32> + Copy,
+{
+    #[inline]
+    fn dist(&self, p: V) -> f32 {
+        let p0 = self.points[0];
+        let p1 = self.points[1];
+        let p2 = self.points[2];
+        let e0 = p1 - p0;
+        let e1 = p2 - p1;
+        let e2 = p0 - p2;
+
+        let v0 = p - p0;
+        let v1 = p - p1;
+        let v2 = p - p2;
+
+        let pq0 = v0 - e0 * (v0.dot(e0) / e0.dot(e0)).clamp(0., 1.);
+        let pq1 = v1 - e1 * (v1.dot(e1) / e1.dot(e1)).clamp(0., 1.);
+        let pq2 = v2 - e2 * (v2.dot(e2) / e2.dot(e2)).clamp(0., 1.);
+
+        let s = (e0.x() * e2.y() - e0.y() * e2.x()).signum();
+        let d = <V::Vec2 as Vec2<f32>>::new(pq0.dot(pq0), s * (v0.x() * e0.y() - v0.y() * e0.x()))
+            .min(<V::Vec2 as Vec2<f32>>::new(
+                pq1.dot(pq1),
+                s * (v1.x() * e1.y() - v1.y() * e1.x()),
+            ))
+            .min(<V::Vec2 as Vec2<f32>>::new(
+                pq2.dot(pq2),
+                s * (v2.x() * e2.y() - v2.y() * e2.x()),
+            ));
+        -d.x().sqrt() * d.y().signum()
+    }
+}
